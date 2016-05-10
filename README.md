1d60c26c54a99c77a6ad69bdb49afa921fd3fe1d
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2016-05-10 16:46:14 -0300

    Modificações no jogo - Novo background; - Lasers; - Novo tamanho de tela; - Estruturação do código; Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tests Pygame
+
+## Art by:
+- Kenney: http://www.kenney.nl/
+
+## Extras:
+- Pyganim: http://www.pygame.org/project-Pyganim+sprite+animation+module-2106-.html
--- space_shooter.py
+++ space_shooter.py
@@ -3,6 +3,8 @@ __author__ = "Marlon"
 import os
 import pygame
 import random
+import pyganim
+import time
 
 from pygame.locals import RLEACCEL, SRCALPHA
 
@@ -12,9 +14,16 @@ BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 RED   = (255,   0,   0)
 
+
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 640
+
 METEORS_BROWN = ["meteorBrown_big1.png","meteorBrown_big2.png","meteorBrown_big3.png","meteorBrown_big4.png",
             "meteorBrown_med1.png","meteorBrown_med3.png","meteorBrown_small1.png","meteorBrown_small2.png",
             "meteorBrown_tiny1.png","meteorBrown_tiny2.png"]
+
+LASERS = ["laserBlue01.png", "laserBlue02.png", "laserBlue03.png", "laserBlue04.png", "laserBlue05.png",
+          "laserBlue06.png", "laserBlue07.png", "laserBlue08.png", "laserBlue09.png", "laserBlue10.png",]
 
 
 def load_image(name, colorkey=None, alpha=False):
@@ -33,20 +42,40 @@ def load_image(name, colorkey=None, alpha=False):
     return image, image.get_rect()
 
 
-def check_col(obj1, obj2):
-    if pygame.sprite.collide_mask(obj1,obj2) is not None:
-        return True
-    else:
-        return False
+class Meteoros(pygame.sprite.Sprite):
+    def __init__(self, meteor="meteorBrown_big1.png", alpha=True):
+        super().__init__()
+        self.image, self.rect = load_image(os.path.join('PNG', 'Meteors', meteor), alpha=alpha)
+        self.mask = pygame.mask.from_surface(self.image, 0)
+
+    def update(self):
+        """ Called each frame. """
+
+        # Move block down one pixel
+        self.rect.y += 1
+
+        # If block is too far down, reset to top of screen.
+        if self.rect.y > 650:
+            self.reset_pos()
+
+    def reset_pos(self):
+        """ Reset position to the top of the screen, at a random x location.
+        Called by update() or the main program loop if there is a collision.
+        """
+        self.rect.y = random.randrange(-300, -20)
+        self.rect.x = random.randrange(0, SCREEN_WIDTH)
 
 
 class Nave(pygame.sprite.Sprite):
-    def __init__(self, alpha=True):
+    last_shot = 0
+    shot_delay = 0.1;
+    projectile_image = None
+
+    def __init__(self, alpha=True, projectile_list=pygame.sprite.Group()):
         super().__init__()
-
         self.image, self.rect = load_image(os.path.join('PNG', 'playerShip1_blue.png'), alpha=alpha)
-
         self.mask = pygame.mask.from_surface(self.image, 0)
+        self.projectile_list = projectile_list
 
     def update(self):
         key = pygame.key.get_pressed()
@@ -57,99 +86,145 @@ class Nave(pygame.sprite.Sprite):
             self.rect.y -= dist  # move up
         if key[pygame.K_RIGHT]:  # right key
             self.rect.x += dist  # move right
+            self.speed_x = dist
         elif key[pygame.K_LEFT]:  # left key
             self.rect.x -= dist  # move left
+            self.speed_x = dist
+
+        if key[pygame.K_SPACE]:
+            if time.time() - self.last_shot > self.shot_delay:
+                projectile = Projectile(self.rect.center)
+                self.projectile_list.add(projectile)
+                self.last_shot = time.time()
 
 
-class Meteoros(pygame.sprite.Sprite):
-    def __init__(self, meteor, alpha=True):
-        super().__init__()
-        self.image, self.rect = load_image(os.path.join('PNG', 'Meteors', meteor), alpha=alpha)
+class Missile(pygame.sprite.Sprite):
+    speed_x = 0
+    speed_y = -10
 
-        self.mask = pygame.mask.from_surface(self.image, 0)
+    def __init__(self, pos, img="laserBlue01.png", alpha=True):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image(os.path.join('PNG', 'Lasers', img), alpha=alpha)
+        self.rect.center = pos
 
     def update(self):
-        """ Called each frame. """
-
-        # Move block down one pixel
-        self.rect.y += 1
-
-        # If block is too far down, reset to top of screen.
-        if self.rect.y > 410:
-            self.reset_pos()
-
-    def reset_pos(self):
-        """ Reset position to the top of the screen, at a random x location.
-        Called by update() or the main program loop if there is a collision.
-        """
-        self.rect.y = random.randrange(-300, -20)
-        self.rect.x = random.randrange(0, screen_width)
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
 
 
-pygame.init()
-
-# Set the height and width of the screen
-screen_width = 700
-screen_height = 400
-screen = pygame.display.set_mode([screen_width, screen_height])
-
-meteor_list = pygame.sprite.Group()
-
-# This is a list of every sprite. All blocks and the player block as well.
-all_sprites_list = pygame.sprite.Group()
-
-for i in range(10):
-    # This represents a block
-    name = random.choice(METEORS_BROWN)
-    meteor = Meteoros(name)
-
-    # Set a random location for the block
-    meteor.rect.x = random.randrange(screen_width)
-    meteor.rect.y = random.randrange(screen_height)
-
-    # Add the block to the list of objects
-    meteor_list.add(meteor)
-    all_sprites_list.add(meteor)
+class Projectile(Missile):
+    def __init__(self, pos, img="laserBlue01.png", alpha=True):
+        Missile.__init__(self, pos, img, alpha)
+        self.mask = pygame.mask.from_surface(self.image)
 
 
-player = Nave()
-all_sprites_list.add(player)
+class Background():
+    def __init__(self):
+        self.image, self.rect = load_image(os.path.join("PNG", "Backgrounds", "black_m.png"))
+        self.background_size = self.image.get_size()
 
-done = False
 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
+class Game():
 
-score = 0
+    x = 0
+    y = 0
+    x1 = 0
+    score = 0
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+    def __init__(self):
+        self.background = Background()
+        self.w, self.h = self.background.background_size
+        self.y1 = -self.h
+        self.meteor_list = pygame.sprite.Group()
 
-    # Clear the screen
-    screen.fill(WHITE)
+        self.all_sprites_list = pygame.sprite.Group()
 
-    # Calls update() method on every sprite in the list
-    all_sprites_list.update()
+        self.projectile_list = pygame.sprite.Group()
+        for i in range(10):
+            # This represents a block
+            name = random.choice(METEORS_BROWN)
+            meteor = Meteoros(name)
 
-    meteors_hit_list = pygame.sprite.spritecollide(player, meteor_list, False, collided=check_col)
+            # Set a random location for the block
+            meteor.rect.x = random.randrange(SCREEN_WIDTH)
+            meteor.rect.y = 0
 
-    # Check the list of collisions.
-    for block in meteors_hit_list:
-        score += 1
-        print(score)
+            # Add the block to the list of objects
+            self.meteor_list.add(meteor)
+            self.all_sprites_list.add(meteor)
+            shoot_animation = pyganim.PygAnimation([(os.path.join('PNG', 'Lasers', 'laserBlue09.png'), 0.5),
+                                                    (os.path.join('PNG', 'Lasers', 'laserBlue08.png'), 0.5)])
 
-        # Reset block to the top of the screen to fall again.
-        block.reset_pos()
+            self.explosion_shoot = shoot_animation.getCopy()
+            self.explosion_shoot.rate = 0.5
 
-    # Draw all the spites
-    all_sprites_list.draw(screen)
+        self.player = Nave(projectile_list=self.projectile_list)
+        self.all_sprites_list.add(self.player)
 
-    # Limit to 20 frames per second
-    clock.tick(40)
+    def run_game(self, screen):
+        self.y1 += 5
+        self.y += 5
+        screen.blit(self.background.image, (self.x, self.y))
+        screen.blit(self.background.image, (self.x1, self.y1))
+        if self.y > self.h:
+            self.y = -self.h
+        if self.y1 > self.h:
+            self.y1 = -self.h
 
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
+        self.all_sprites_list.update()
+        self.projectile_list.update()
 
-pygame.quit()
+        for projectile in self.projectile_list:
+            if projectile.rect.x < 0 or projectile.rect.x > SCREEN_WIDTH:
+                self.projectile_list.remove(projectile)
+            elif projectile.rect.y < - 20 or projectile.rect.y > SCREEN_HEIGHT:
+                self.projectile_list.remove(projectile)
+
+        meteors_hit_list = pygame.sprite.spritecollide(self.player, self.meteor_list, False, pygame.sprite.collide_mask)
+        shoot_laser_list = pygame.sprite.groupcollide(self.projectile_list, self.meteor_list, False, False,
+                                                      pygame.sprite.collide_mask)
+
+        for proj, meteor in shoot_laser_list.items():
+            self.projectile_list.remove(proj)
+            self.explosion_shoot.play()
+            self.explosion_shoot.blit(screen, (proj.rect.x, proj.rect.y))
+            proj.kill()
+            for m in meteor:
+                m.reset_pos()
+
+        for block in meteors_hit_list:
+            self.score += 1
+            print(self.score)
+
+            block.reset_pos()
+
+        self.all_sprites_list.draw(screen)
+        self.projectile_list.draw(screen)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    pygame.display.set_caption("SPACE SHOOTER")
+
+    done = False
+
+    clock = pygame.time.Clock()
+    game = Game()
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        game.run_game(screen)
+
+        pygame.display.update()
+        pygame.display.flip()
+        clock.tick(40)
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
